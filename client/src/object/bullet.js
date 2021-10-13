694e9bf8a97ed6f1955a4ab9fe6052b4ca8b34d0
nameeeeeee <779427235@qq.com>
2021-10-13 17:41:49 +0800

    1.8 子弹数组貌似也好了

--- client/src/object/bullet.js
+++ client/src/object/bullet.js
@@ -16,7 +16,7 @@ Bullet.yLength = 4
 Bullet.speed = 10
 Bullet.prototype.speed = 10
 
-const bullet = []
+let bullet = []
 
 // 计算出diff，封装成对象发送
 Bullet.send = function() {
@@ -43,14 +43,7 @@ Bullet.createRegister = function() {
     const li = document.createElement('li')
     li.className = 'bullet'
     objectDOM.appendChild(li)
-
-    // 若检测到空这创建
-    for (let i = 0; i <= bullet.length; i++) {
-      if (!bullet[i]) {
-        bullet[i] = new Bullet(data.name, data.xInit, data.yInit, data.xDiff, data.yDiff, li)
-        break
-      }
-    }
+    bullet[bullet.length] = new Bullet(data.name, data.xInit, data.yInit, data.xDiff, data.yDiff, li)
   })
 }
 
@@ -58,55 +51,40 @@ const objectDOM = document.querySelector('.object')
 
 // 遍历子弹对象，有长度控制和碰墙检测
 Bullet.move = function() {
-  for (let i = 0; i < bullet.length; i++) {
-    if (bullet[i]) {
-      let xTo = bullet[i].x + bullet[i].xDiff
-      let yTo = bullet[i].y + bullet[i].yDiff
+  bullet = bullet.filter(itemBullet => {
+    let xTo = itemBullet.x + itemBullet.xDiff
+    let yTo = itemBullet.y + itemBullet.yDiff
+    let isTouchPlayer = false
 
-      let isTouchPlayer = false
-
-      for (let j = 0; j < player.length; j++) {
-
-        if (player[j]) { //数据不为空
-          // console.log(player[j].name, bullet[i].name, player[j].name !== bullet[i].name);
-          
-          if (player[j].name !== bullet[i].name) { // 该玩家不为当前子弹发送者，需判断碰撞
-            if (
-              Math.abs(player[j].x - bullet[i].x) < player[j].xLength / 2 + Bullet.xLength / 2
-              &&
-              Math.abs(player[j].y - bullet[i].y) < player[j].yLength / 2 + Bullet.yLength / 2
-            ) {
-              // console.log('for');
-              // console.log('touch');
-              isTouchPlayer = true
-              break
-            }
-          }
+    player.some(itemPlayer => {
+      if (itemPlayer.name !== itemBullet.name) { // 该玩家不为当前子弹发送者，需判断碰撞
+        if (
+          Math.abs(itemPlayer.x - itemBullet.x) < itemPlayer.xLength / 2 + Bullet.xLength / 2
+          &&
+          Math.abs(itemPlayer.y - itemBullet.y) < itemPlayer.yLength / 2 + Bullet.yLength / 2
+        ) {
+          isTouchPlayer = true
+          return true
         }
-
       }
+    })
 
-      // 碰墙检测 击中检测
-      if (xTo < 0 || xTo > 600 || yTo < 0 || yTo > 600 || isTouchPlayer) {
-        objectDOM.removeChild(bullet[i].dom)
-        bullet[i] = undefined
+    // 碰墙检测 击中检测
+    if (xTo < 0 || xTo > 600 || yTo < 0 || yTo > 600 || isTouchPlayer) {
+      objectDOM.removeChild(itemBullet.dom)
+      console.log(bullet);
+      return false  // 直接排除该数据
+    } else {
+      // 正常移动
+      itemBullet.x = xTo
+      itemBullet.y = yTo
 
-        // 动态删除后面的undefined
-        for (let i = bullet.length - 1; i >= 0; i--) {
-          if (!bullet[i]) bullet.splice(i, 1);
-          else break
-        }
-      } else {
-        // 正常移动
-        bullet[i].x = xTo
-        bullet[i].y = yTo
+      itemBullet.dom.style.left = xTo - Bullet.xLength / 2 + 'px'   // 根据边长换算出left和top
+      itemBullet.dom.style.top = yTo - Bullet.yLength / 2 + 'px'
 
-        bullet[i].dom.style.left = xTo - Bullet.xLength / 2 + 'px'   // 根据边长换算出left和top
-        bullet[i].dom.style.top = yTo - Bullet.yLength / 2 + 'px'
-      }
-
+      return true
     }
-  }
+  })
 }
 
 export { Bullet, bullet }
