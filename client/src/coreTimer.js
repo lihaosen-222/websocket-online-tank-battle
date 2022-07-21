979fd6d89e48fc0f7ac56b4d60e9a3bda337c487
lihaosen <779427235@qq.com>
2022-07-21 21:29:17 +0800

    fix: 修复pc端射击逻辑

--- client/src/coreTimer.js
+++ client/src/coreTimer.js
@@ -2,20 +2,23 @@ import KeyBoard from './object/Keyboard'
 import Tap from './object/Tap'
 import { checkMobile } from './utils'
 
-
 const isMobile = checkMobile()
+const keyBoard = new KeyBoard()
+const tap = new Tap('.shell', isMobile ? 'mobile' : 'pc')
 
-export default function startTimer(config ) {
-  const {socket, otherTanks, gameMap, myTank} = config
+function shouldMove(config) {
+  const {tap, keyBoard, isMobile} = config
+  return keyBoard.getIsDown() || (isMobile && tap.getIsDown())
+}
 
-  const keyBoard = new KeyBoard()
-  const tap = new Tap('.shell', isMobile ? 'mobile' : 'pc')
+export default function startTimer(config) {
+  const { socket, otherTanks, gameMap, myTank } = config
 
   return setInterval(function () {
     const { xPos, yPos } = myTank.getPostion()
     if (tap.getIsDown()) myTank.fire()
 
-    if (keyBoard.getIsDown() || tap.getIsDown()) {
+    if (shouldMove({keyBoard, tap, isMobile})) {
       let { xPos: xTo, yPos: yTo } = myTank.getNextPostion()
       xTo = gameMap.isXCollided(xTo) ? xPos : xTo
       yTo = gameMap.isYCollided(yTo) ? yPos : yTo
